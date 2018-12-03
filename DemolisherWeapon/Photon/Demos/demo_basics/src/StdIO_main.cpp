086104724d808caaf3da5e4958afef5bd9d8d865
ShanzhongXinzhijie <44658366+ShanzhongXinzhijie@users.noreply.github.com>
2018-12-03 12:11:15 +0900

    photon

--- DemolisherWeapon/Photon/Demos/demo_basics/src/StdIO_main.cpp
+++
@@ -1,7 +0,0 @@
-#include "BasicsApplication.h"
-#include "StdIO_UIListener.h"
-
-int main(void)
-{
-	BasicsApplication::run(new StdIO_UIListener());
-}
--- DemolisherWeapon/Photon/Demos/demo_particle/cocos2dx/windows/copyResources.cmd
+++
@@ -1,9 +0,0 @@
-set dest=%1%
-set cocos2dLibPath=..\..\..\etc-bin\cocos2dx\cocos2dx\out\%dest%\
-
-copy %cocos2dLibPath%*.dll %dest% /Y
-
-copy ..\resources\*.* %dest% /Y
-goto end
-
-:end
