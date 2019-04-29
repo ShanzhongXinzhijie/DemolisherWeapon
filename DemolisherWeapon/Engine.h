72549955d9b19061946f4f6abb638416eb4865ae
ShanzhongXinzhijie <44658366+ShanzhongXinzhijie@users.noreply.github.com>
2019-04-30 02:21:28 +0900

    VerUp

--- DemolisherWeapon/Engine.h
+++ DemolisherWeapon/Engine.h
@@ -23,7 +23,7 @@
 
 namespace DemolisherWeapon {
 
-static constexpr wchar_t DW_VER[] = L"NewMat";//エンジンのバージョン
+static constexpr wchar_t DW_VER[] = L"グレア";//エンジンのバージョン
 
 enum EnSplitScreenMode {
 	enNoSplit=0,
