669e748512d5a08399a110ef5d142cd2933b7f2f
ShanzhongXinzhijie <44658366+ShanzhongXinzhijie@users.noreply.github.com>
2019-05-21 09:51:18 +0900

    HLSL定数バッファの注意文追加

--- DemolisherWeapon/Graphic/Shader/ShaderUtil.h
+++ DemolisherWeapon/Graphic/Shader/ShaderUtil.h
@@ -5,7 +5,14 @@ namespace DemolisherWeapon {
 	class ShaderUtil
 	{
 	public:
-		//定数バッファの作成
+		/// <summary>
+		/// 定数バッファの作成
+		/// サイズが16バイトの倍数になるように確保される。
+		/// ※なお、データは16バイト境界を跨いではならない。
+		/// https://www.3dgep.com/texturing-lighting-directx-11/#Packing_Rules_for_Constant_Buffers
+		/// </summary>
+		/// <param name="size">定数バッファのサイズ</param>
+		/// <param name="buffer">定数バッファのポインタが返ってくる</param>
 		static void CreateConstantBuffer(int size, ID3D11Buffer** buffer);
 	};
 
