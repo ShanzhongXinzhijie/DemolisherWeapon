cdd02de2ae646a8a6f9e19b00d8c9b084fb13cec
ShanzhongXinzhijie <44658366+ShanzhongXinzhijie@users.noreply.github.com>
2020-06-05 15:10:17 +0900

    途中

--- DemolisherWeapon/system/GraphicsEngine.cpp
+++ DemolisherWeapon/system/GraphicsEngine.cpp
@@ -74,6 +74,244 @@ void GraphicsEngine::Release()
 
 	m_FRT.Release();
 }
+
+UINT CreateDescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE type, UINT numDescriptors, bool isShaderVisible, Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>& descriptorHeap) {
+	D3D12_DESCRIPTOR_HEAP_DESC desc = {};
+	desc.Type = type;
+	desc.NumDescriptors = numDescriptors;
+	desc.Flags = isShaderVisible ? D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE : D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	if (FAILED(device->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&descriptorHeap)))) {
+		return -1;
+	}
+	//ディスクリプタのサイズを返す
+	return device->GetDescriptorHandleIncrementSize(type);
+}
+
+bool DX12Init() {
+	using namespace Microsoft::WRL;
+
+	UINT dxgiFactoryFlags = 0;
+#ifndef DW_MASTER
+	{
+		//デバッグコントローラーがあれば、デバッグレイヤーがあるDXGIを作成する。
+		ComPtr<ID3D12Debug> debugController;
+		if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+		{
+			debugController->EnableDebugLayer();
+			// Enable additional debug layers.
+			dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+		}
+	}
+#endif
+	//DXGIFactory作成
+	ComPtr<IDXGIFactory4> dxgiFactory;
+	if (FAILED(CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&dxgiFactory)))) {
+		return false;
+	}
+
+	// 機能レベル11を満たすハードウェアアダプタを検索し、そのデバイスインターフェイスを取得する.
+	ComPtr<IDXGIAdapter1> dxgiAdapter, dxgiAdapterTmp; // デバイス情報を取得するためのインターフェイス
+	int adapterIndex = 0; // 列挙するデバイスのインデックス
+	SIZE_T maxDedicatedVideoMemory = 0;//トップのビデオメモリ
+	//ループで探す
+	while (dxgiFactory->EnumAdapters1(adapterIndex, &dxgiAdapterTmp) != DXGI_ERROR_NOT_FOUND) {
+		//情報取得
+		DXGI_ADAPTER_DESC1 desc;
+		dxgiAdapterTmp->GetDesc1(&desc);
+
+		//TODO アダプタ、オプションで選択できるように
+
+		//HardwareAdapterを使用する & ビデオメモリがより多いもの
+		if (!(desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) && desc.DedicatedVideoMemory >= maxDedicatedVideoMemory) {			
+			dxgiAdapter = dxgiAdapterTmp;
+			maxDedicatedVideoMemory = desc.DedicatedVideoMemory;
+		}
+		++adapterIndex;
+	}
+
+	// 機能レベル11を満たすハードウェアが見つからない場合、WARPデバイスの作成を試みる.
+	if (!dxgiAdapter) {
+		if (FAILED(dxgiFactory->EnumWarpAdapter(IID_PPV_ARGS(&dxgiAdapter)))) {
+			return false;//もうだめだぁー(BNBN)
+		}
+	}
+
+	//使用する機能レベル
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_12_1,
+		D3D_FEATURE_LEVEL_12_0,
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+	};
+
+	//TODO
+	ComPtr<ID3D12Device> m_d3dDevice;
+	ComPtr<ID3D12CommandQueue> m_commandQueue;
+
+	//D3D12デバイスの作成
+	for (auto fuatureLevel : featureLevels) {
+		if (SUCCEEDED(D3D12CreateDevice(dxgiAdapter.Get(), fuatureLevel, IID_PPV_ARGS(&m_d3dDevice)))) {
+			break;
+		}
+	}
+
+	// コマンドキューを作成
+	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+	auto hr = m_d3dDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_commandQueue));
+	if (FAILED(hr)) {
+		return false;
+	}
+
+	// スワップチェーンを作成
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+	swapChainDesc.BufferCount = FRAME_COUNT;
+	swapChainDesc.Width = initParam.frameBufferWidth;
+	swapChainDesc.Height = initParam.frameBufferHeight;
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	swapChainDesc.SampleDesc.Count = 1;
+
+	ComPtr<IDXGISwapChain1> swapChainTmp;
+	auto hr = dxgiFactory->CreateSwapChainForHwnd(
+		m_commandQueue.Get(),
+		hwnd,
+		&swapChainDesc,
+		nullptr,
+		nullptr,
+		&swapChainTmp
+	);
+	if (FAILED(hr)) {
+		return false;
+	}
+
+	//IDXGISwapChain3のインターフェースを取得。
+	swapChainTmp.As(&m_swapChain);
+	m_currentBackBufferIndex = m_swapChain->GetCurrentBackBufferIndex();
+
+
+	//RTV用のデスクリプタヒープ作成
+	auto rtvDescriptorSize = CreateDescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE_RTV, FRAME_COUNT, false, m_rtvDescriptorHeap);
+
+	//RTV作成
+	D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = m_rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+	for (int i = 0; i < FRAME_COUNT; ++i) {
+		auto hr = m_swapChain->GetBuffer(i, IID_PPV_ARGS(&m_renderTargets[i]));
+		if (FAILED(hr)) {
+			return false;
+		}
+		device->CreateRenderTargetView(m_renderTargets[i].Get(), nullptr, rtvHandle);
+		rtvHandle.ptr += rtvDescriptorSize;
+	}
+
+
+	/*
+
+	// コマンドアロケータを作成する.
+	for (int i = 0; i < frameBufferCount; ++i) {
+		if (FAILED(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator[i])))) {
+			return false;
+		}
+	}
+
+	// コマンドリストを作成する.
+	if (FAILED(device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator[currentFrameIndex].Get(), nullptr, IID_PPV_ARGS(&commandList)))) {
+		return false;
+	}
+	if (FAILED(commandList->Close())) {
+		return false;
+	}
+
+	// フェンスとフェンスイベントを作成する.
+	if (FAILED(device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence)))) {
+		return false;
+	}
+	fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+	if (!fenceEvent) {
+		return false;
+	}
+	for (int i = 0; i < frameBufferCount; ++i) {
+		fenceValue[i] = 0;
+	}
+
+	/// <summary>
+	/// ///////////
+	/// </summary>
+	/// 
+	//フレームバッファ用のDSVを作成する。
+	if (!CreateDSVForFrameBuffer(initParam)) {
+		TK_ASSERT(false, "フレームバッファ用のDSVの作成に失敗しました。");
+		return false;
+	}
+	//コマンドアロケータの作成。
+	m_d3dDevice->CreateCommandAllocator(
+		D3D12_COMMAND_LIST_TYPE_DIRECT,
+		IID_PPV_ARGS(&m_commandAllocator));
+	if (!m_commandAllocator) {
+		TK_ASSERT(false, "コマンドアロケータの作成に失敗しました。");
+		return false;
+	}
+
+	//コマンドリストの作成。
+	if (!CreateCommandList()) {
+		TK_ASSERT(false, "コマンドリストの作成に失敗しました。");
+		return false;
+	}
+	//GPUと同期をとるためのオブジェクトを作成する。
+	if (!CreateSynchronizationWithGPUObject()) {
+		TK_ASSERT(false, "GPUとの同期オブジェクトの作成に失敗しました。");
+		return false;
+	}
+	//メインレンダリングターゲットを作成。
+	float clearColor[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+	if (m_mainRenderTarget.Create(
+		initParam.frameBufferWidth,
+		initParam.frameBufferHeight,
+		1,
+		1,
+		DXGI_FORMAT_R32G32B32A32_FLOAT,
+		DXGI_FORMAT_D32_FLOAT,
+		clearColor) == false) {
+		TK_ASSERT(false, "メインレンダリングターゲットの作成に失敗しました。");
+		return false;
+	}
+
+	//CBR_SVRのディスクリプタのサイズを取得。
+	m_cbrSrvDescriptorSize = m_d3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+
+	//パイプラインステートを初期化。
+	CPipelineStatesDx12::Init();
+
+	//ポストエフェクトの初期化。
+	m_postEffect.Init(initParam.graphicsConfing);
+
+	m_copyFullScreenSprite.Init(
+		&m_mainRenderTarget.GetRenderTargetTexture(),
+		static_cast<float>(initParam.frameBufferWidth),
+		static_cast<float>(initParam.frameBufferHeight));
+	//ビューポートを初期化。
+	m_viewport.TopLeftX = 0;
+	m_viewport.TopLeftY = 0;
+	m_viewport.Width = static_cast<float>(initParam.frameBufferWidth);
+	m_viewport.Height = static_cast<float>(initParam.frameBufferHeight);
+	m_viewport.MinDepth = D3D12_MIN_DEPTH;
+	m_viewport.MaxDepth = D3D12_MAX_DEPTH;
+
+	//シザリング矩形を初期化。
+	m_scissorRect.left = 0;
+	m_scissorRect.top = 0;
+	m_scissorRect.right = initParam.frameBufferWidth;
+	m_scissorRect.bottom = initParam.frameBufferHeight;
+
+	//レンダリングコンテキストの作成。
+	auto giFactry = g_engine->GetGraphicsInstanceFactory();
+	m_renderContext = giFactry->CreateRenderContext();
+	auto& rcDx12 = m_renderContext->As<CRenderContextDx12>();
+	rcDx12.SetCommandList(m_commandList);
+	*/
+}
+
 void GraphicsEngine::Init(HWND hWnd, const InitEngineParameter& initParam)
 {
 	FRAME_BUFFER_W = (float)initParam.frameBufferWidth;
@@ -98,9 +336,10 @@ void GraphicsEngine::Init(HWND hWnd, const InitEngineParameter& initParam)
 	//この配列はD3D11CreateDeviceAndSwapChainの引数として使う。
 	D3D_FEATURE_LEVEL featureLevels[] =
 	{
+		D3D_FEATURE_LEVEL_12_1,
+		D3D_FEATURE_LEVEL_12_0,
+		D3D_FEATURE_LEVEL_11_1,
 		D3D_FEATURE_LEVEL_11_0,
-		//D3D_FEATURE_LEVEL_10_1,
-		//D3D_FEATURE_LEVEL_10_0,
 	};
 
 	//D3Dデバイスとスワップチェインを作成する。
