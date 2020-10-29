4939b660eab81d258746a789f27d240e1c572a6b
ShanzhongXinzhijie <44658366+ShanzhongXinzhijie@users.noreply.github.com>
2020-10-29 13:16:19 +0900

    スプライトレンダー追加

--- DemolisherWeapon/Graphic/Sprite/CSprite.h
+++ DemolisherWeapon/Graphic/Sprite/CSprite.h
@@ -62,4 +62,76 @@ namespace DemolisherWeapon {
 		RECT m_sourceRectangle;
 	};
 
+	class CSpriteRender : public IGameObject
+	{
+	public:
+		void Init(const wchar_t* fileName) {
+			m_sprite.Init(fileName);
+		}
+
+		//ステータス設定
+		void SetPos(const CVector2& pos) {
+			m_pos = pos;
+		}
+		void SetScale(const CVector2& scale) {
+			m_scale = scale;
+		}
+		void SetPivot(const CVector2& pivot) {
+			m_pivot = pivot;
+		}
+		void SetRot(float rot) {
+			m_rotation = rot;
+		}
+		void SetColor(const CVector4& color) {
+			m_color = color;
+		}
+
+		/// <summary>
+		/// スクリーン座標で位置を指定するか
+		/// 右下の座標=画面解像度
+		/// </summary>
+		/// <param name="isuse"></param>
+		void SetUseScreenPos(bool isuse) {
+			m_isUseScreenPos = isuse;
+		}
+
+		//画像の描画する範囲を設定
+		//0.0f～1.0fの範囲で指定
+		void SetSourceRectangle(const float left, const float top, const float right, const float bottom) {
+			m_sprite.SetSourceRectangle(left, top, right, bottom);
+		}
+		//画像の解像度で指定
+		void SetSourceRectangleSpriteSize(const RECT& sourceRectangle) {
+			m_sprite.SetSourceRectangleSpriteSize(sourceRectangle);
+		}
+
+		void PostRender()override {
+			if (!m_isUseScreenPos) {
+				m_sprite.Draw(m_pos,
+					m_scale,
+					m_pivot,
+					m_rotation,
+					m_color
+				);
+			}
+			else {
+				m_sprite.DrawScreenPos(m_pos,
+					m_scale,
+					m_pivot,
+					m_rotation,
+					m_color
+				);
+			}
+		}
+
+	private:
+		CSprite m_sprite;
+
+		bool m_isUseScreenPos = false;
+		CVector2 m_pos;
+		CVector2 m_scale = CVector2::One();
+		CVector2 m_pivot = CVector2::One() * 0.5f;
+		float m_rotation = 0.0f;
+		CVector4 m_color = CVector4::White();
+	};
 }
